32b2dc408ea3402941d2232f6424395d701a1d5b
Md. Kamrul Huda - BGIT / ACC <kamrul.huda@gmail.com>
2024-05-16 14:26:22 +0600

    completed detail page

--- src/Components/BookDetail/BookDetail.jsx
+++ src/Components/BookDetail/BookDetail.jsx
@@ -54,22 +54,70 @@ const BookDetail = () => {
   } = book;
 
   return (
-    <div className="flex justify-between items-center">
-      <div className="bg-red-300">
-        <img className="bg-[#1313130D h-[400px]" src={image} alt="" />
+    <div className="flex justify-between items-center mt-10 gap-6 min-h-[400px]">
+      <div className="w-1/2 bg-[#1313130D] rounded-2xl flex justify-center h-full">
+        <img className="bg-[#F3F3F3] h-full object-fill" src={image} alt="" />
       </div>
-      <div>
-        <p>{bookName}</p>
-        <p>{author}</p>
-        <p>{category}</p>
-        <p>{review}</p>
-        <p>{tags}</p>
-        <p>{totalPages}</p>
-        <p>{publisher}</p>
-        <p>{yearOfPublishing}</p>
-        <p>{rating}</p>
-        <button onClick={handleReadBook}>Read</button>
-        <button onClick={handleWishlistBook}>Wishlist</button>
+      <div className="w-1/2 space-y-3 h-full">
+        <p className="text-[##131313] font-bold text-4xl">{bookName}</p>
+        <p className="text-[#131313CC] font-medium text-xl">By: {author}</p>
+        <hr />
+        <p className="text-[#131313CC] font-medium text-xl">{category}</p>
+        <hr />
+        <p className=".workSans text-[#131313B3]">
+          <b>Review: </b> {review}
+        </p>
+        <div className="flex  items-center gap-4 text-left">
+          <b> Tag:</b>
+          {tags.map((tag, i) => (
+            <p
+              key={i}
+              className="rounded-full text-[#23BE0A] bg-[#23BE0A0D] font-medium  .workSans py-1"
+            >
+              #{tag}
+            </p>
+          ))}
+        </div>
+        <hr />
+
+        <div className="flex gap-5">
+          <div className=".workSans text-[#131313B3] flex flex-col gap-3">
+            <div> Number of pages:</div>
+            <div> Publisher:</div>
+            <div> Year of Publishing:</div>
+            <div> Rating:</div>
+          </div>
+          <div className=".workSans text-[#131313B3] flex flex-col gap-3">
+            <div>
+              <b>{totalPages}</b>
+            </div>
+            <div>
+              <b>{publisher}</b>
+            </div>
+            <div>
+              <b>{yearOfPublishing}</b>
+            </div>
+            <div>
+              <b>{rating}</b>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-start gap-4">
+          {/* <button onClick={handleReadBook} className="btn bor">
+            Read
+          </button> */}
+          <button className="btn bg-transparent hover:bg-transparent hover:scale-105 text-[#131313] font-semibold text-xl border-solid border-[#1313134D] .workSans">
+            Read
+          </button>
+          <button
+            onClick={handleWishlistBook}
+            className="btn bg-[#50B1C9] text-white hover:bg-[#50B1C9] font-semibold text-xl hover:scale-105 .workSans"
+          >
+            Wishlist
+          </button>
+        </div>
+
         <ToastContainer />
       </div>
     </div>
